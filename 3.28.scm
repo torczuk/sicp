5cf849b0b0a187206be885eef51b0c9dfa439723
torczuk <torczuk@gmail.com>
2018-09-12 09:35:20 +0800

    Create 3.28.scm

---
+++ 3.28.scm
@@ -0,0 +1,16 @@
+(define or-gate-delay 10)
+
+(define (logical-or a b)
+ (cond ((equal? a 1) 1)
+  ((equal? b 1) 1)
+  (else 0)))
+
+(define (or-gate a1 a2 output)
+ (define (or-action-procedure)
+  (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+   (after-delay or-gate-delay
+    (lambda () (set-signal! output new-value)))))
+
+ (add-action! a1 or-action-procedure)
+ (add-action! a2 or-action-procedure)
+ 'ok)
