52823ba6e67ad89c6a263859e9d6efb1abd7f53c
torczuk <torczuk@gmail.com>
2018-09-13 11:38:25 +0800

    Update 3.33.scm

--- 3.33.scm
+++ 3.33.scm
@@ -167,20 +167,6 @@
  ((connector 'connect) new-constraint))
 
 
-(define (celsius-fahrenheit-converter c f)
- (let ((u (make-connector))
-       (v (make-connector))
-       (w (make-connector))
-       (x (make-connector))
-       (y (make-connector)))
-  (multiplier c w u)
-  (multiplier v x u)
-  (adder v y f)
-  (constant 9 w)
-  (constant 5 x)
-  (constant 32 y)
-  'ok))
-
 (define (averager a b c)
  (let ((u (make-connector))
        (v (make-connector))
@@ -192,7 +178,7 @@
   (constant 2 y)
   'ok))
 
-
+; test
 (define A (make-connector))
 (define B (make-connector))
 (define C (make-connector))
