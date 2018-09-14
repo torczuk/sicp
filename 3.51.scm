e1cb08d39a5f19df628fa589c0cf5589a997d7c6
torczuk <torczuk@gmail.com>
2018-09-14 15:32:48 +0800

    Create 3.51.scm

---
+++ 3.51.scm
@@ -0,0 +1,30 @@
+(define (stream-ref s n)
+ (if (= n 0)
+  (stream-car s)
+  (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+ (if (stream-null? s)
+  the-empty-stream
+  (cons-stream (proc (stream-car s))
+   (stream-map proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+ (if (> low high)
+  the-empty-stream
+  (cons-stream
+   low
+   (stream-enumerate-interval (+ low 1) high))))
+
+(define (show x)
+ (display x)
+ x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+(stream-ref x 5)
+;; 12345
+;; Value: 5
+
+(stream-ref x 7)
+;; 67
+;; Value: 7
