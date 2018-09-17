0326a1d9c4d65795942fe3b6fcf8b9ce197ba1ab
torczuk <torczuk@gmail.com>
2018-09-17 15:19:12 +0800

    Create 3.66.scm

---
+++ 3.66.scm
@@ -0,0 +1,17 @@
+(define (interleave s t)
+ (if (null-stream? s)
+  t
+  (cons-stream (stream-car s)
+   (interleave t (stream-cdr s)))))
+
+(define (pair s t)
+ (cons-stream
+  (list (stream-car s) ((stream-car t)))
+  (interleave
+   (stream-map (lambda (x) (list (stream-car s) x))
+    (stream-cdr t))
+   (pairs (stream-cdr s) (stream-cdr t)))))
+
+
+;; number of pairs before (n, m) pair
+;; (1 + (m -1) / 2) * (m  - 1) + (n - 1)
